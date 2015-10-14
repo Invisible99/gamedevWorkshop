5d8b28da395009040f8a1c48c025d8e336478867
MrInvisible99 <theinvisible99@hotmail.com>
2015-10-14 10:13:53 +0200

    Project aanmaken

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+    public float speed;
+    private Transform trans;
+    // Use this for initialization
+    void Start() {
+        trans = GetComponent<Transform>() as Transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        trans.position = trans.position + new Vector3(speed * Time.deltaTime, 0, 0);
+	}
+}
