54cde1ea651f635f1c94f1a8f91ec1f910d8360a
Audric de Schaetzen <audric.deschaetzen+github@gmail.com>
2015-10-14 10:57:40 +0200

    MEssed around with character controllers

---
+++ Assets/CharacterControllerMessAround/Scripts/Player1Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour
+{
+	public float Speed;
+	public float Gravity;
+
+	CharacterController motor;
+	// Use this for initialization
+	void Start()
+	{
+	motor = GetComponent<CharacterController>() as CharacterController;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		var direction = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Jump"), Input.GetAxis("Vertical"));
+
+		motor.Move(new Vector3(direction.x * Speed * Time.deltaTime, direction.y * Speed * Time.deltaTime, direction.z * Speed * Time.deltaTime));
+
+		/*if (direction != Vector3.zero)
+		{
+			this.transform.position += new Vector3(direction.x * Speed * Time.deltaTime, direction.y*Speed*Time.deltaTime, direction.z * Speed * Time.deltaTime);
+		}*/
+	}
+}
